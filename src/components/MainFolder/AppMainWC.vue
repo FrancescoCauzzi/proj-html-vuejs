250f33c1961b45611c310f6c1698432d108b0cf5
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-04-11 16:23:54 +0200

    feat: first block of the main added

--- src/components/MainFolder/AppMainWC.vue
+++ src/components/MainFolder/AppMainWC.vue
@@ -7,11 +7,15 @@ export default {
 };
 </script>
 <template>
-  <div class="container-fluid __container-outer">
-    <div class="container __container-inner">
+  <div class="container-fluid __container-outer text-center py-5">
+    <div
+      class="container __container-inner d-flex align-items-center flex-column"
+    >
       <h2 class="text-white">WORLD CLASS Fitness</h2>
-      <div class="__stars"></div>
-      <p>
+      <div class="__stars d-flex gap-2 py-4">
+        <i class="fa-regular fa-star"></i><i class="fa-regular fa-star"></i>
+      </div>
+      <p class="__paragraph w-50">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
         autem assumenda quisquam repudiandae et ea! A nemo rerrum.
       </p>
@@ -21,5 +25,13 @@ export default {
 <style scoped>
 .__container-outer {
   background-color: #25292b;
+}
+
+.__stars {
+  color: gold;
+}
+
+.__paragraph {
+  color: #aaa9a9;
 }
 </style>
