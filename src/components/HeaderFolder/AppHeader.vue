6f1589ea6c8c07da03d9b3037d05e647dc94fe17
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-04-11 15:52:32 +0200

    feat: header done

--- src/components/HeaderFolder/AppHeader.vue
+++ src/components/HeaderFolder/AppHeader.vue
@@ -11,8 +11,17 @@ export default {
 };
 </script>
 <template>
-  <div class="container-fluid __container-outer">
+  <div class="container-fluid __container-outer py-3">
     <AppHeaderNavbar></AppHeaderNavbar>
+    <div class="__header-title d-flex flex-column align-items-center gap-3">
+      <h1 class="text-uppercase fw-bold text-white">no pain no gain</h1>
+      <h5 class="text-white">
+        Motivation is what gets you started. Habit is what keeps you going.
+      </h5>
+      <button type="button" class="btn btn-outline-secondary">
+        Join us today
+      </button>
+    </div>
   </div>
 </template>
 <style scoped>
@@ -20,7 +29,12 @@ export default {
   background-image: url(../../../public/img/home-banner.jpg);
   background-size: cover;
   background-repeat: no-repeat;
-  height: 100vh;
+
   width: 100%;
+}
+
+.__header-title {
+  padding: 140px 0 140px 0;
+  margin-bottom: 100px;
 }
 </style>
