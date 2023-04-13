a39d93123b6bb44a2b18de118b38287b6d2f5b0c
FrancescoCauzzi <elblanco.fc@gmail.com>
2023-04-13 09:37:16 +0200

    feat: burger menu on the navbar added

--- src/components/HeaderFolder/AppHeaderNavbar.vue
+++ src/components/HeaderFolder/AppHeaderNavbar.vue
@@ -30,36 +30,64 @@ export default {
 };
 </script>
 <template>
-  <div class="text-white">
-    <div
-      class="container __nav-container d-flex justify-content-between flex-wrap"
-    >
+  <nav class="text-white navbar navbar-expand-lg navbar-light">
+    <div class="container __nav-container d-flex justify-content-between">
       <div class="__logo mb-4">
         <img src="/img/gym_logo_1x.png" alt="Cannot retrieve image" srcset="" />
       </div>
-      <div class="__links d-flex align-items-center justify-content-center">
-        <ul class="d-flex gap-3 fw-bold flex-wrap justify-content-start">
-          <li v-for="(item, index) in links">
-            <a
-              :class="this.active === index ? '__active' : ''"
-              :href="`#section${item.id}`"
-              @click="
-                activateLink(index),
-                  scrollFunction(`section${item.id}`) in links
-              "
-              >{{ item.name.toUpperCase() }}</a
-            >
-          </li>
-        </ul>
+      <!-- links che devono essere inseriti nel burger menu -->
+
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div
+        class="collapse navbar-collapse text-white justify-content-end p-2"
+        id="navbarSupportedContent"
+      >
+        <div class="__links d-flex align-items-center justify-content-center">
+          <ul class="d-flex gap-3 fw-bold flex-wrap justify-content-center">
+            <li v-for="(item, index) in links">
+              <a
+                :class="this.active === index ? '__active' : ''"
+                :href="`#section${item.id}`"
+                @click="
+                  activateLink(index),
+                    scrollFunction(`section${item.id}`) in links
+                "
+                >{{ item.name.toUpperCase() }}</a
+              >
+            </li>
+          </ul>
+        </div>
       </div>
+      <!--  -->
     </div>
-  </div>
+  </nav>
 </template>
 <style scoped>
 /* .__links {
   position: fixed;
   right: 20px;
 } */
+
+.navbar-toggler {
+  color: white;
+  background-color: #454d51;
+  border-color: white;
+}
+
+.navbar-toggler-icon {
+  color: white;
+}
+
 a {
   text-decoration: none;
   color: white;
